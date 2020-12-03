f3fd9379143cefda8fc623ee0e2bd5673d57a52f
Daniel Podsadowski <pods0005@algonquinlive.com>
2020-12-03 14:33:41 -0500

    Prepare for Day 3

---
+++ AdventOfCode2020/Day/Day3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    public class Day3
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\day3.txt";
+            List<String> list = new List<String>();
+            var lines = File.ReadAllLines(fileInput);
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                list.Add(lines[i]);
+            }
+
+            Console.WriteLine("Commencing Day3 A...");
+            //idk
+        }
+    }
+}
