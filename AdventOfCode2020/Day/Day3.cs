0ed1b092bc59f434eeb84e271f34b54eeab47dae
Daniel Podsadowski <pods0005@algonquinlive.com>
2020-12-03 20:08:24 -0500

    Day 3 Completed

--- AdventOfCode2020/Day/Day3.cs
+++ AdventOfCode2020/Day/Day3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
@@ -8,17 +9,39 @@ namespace AdventOfCode2020
     {
         public static void Main()
         {
-            var fileInput = @"..\..\..\Data\day3.txt";
-            List<String> list = new List<String>();
-            var lines = File.ReadAllLines(fileInput);
-
-            for (var i = 0; i < lines.Length; i++)
-            {
-                list.Add(lines[i]);
-            }
+            var lines = File.ReadLines(@"..\..\..\Data\day3.txt").ToArray();
+            var numOfColumns = lines[0].Length;
 
             Console.WriteLine("Commencing Day3 A...");
-            //idk
+            var treeCount = TraverseMap(lines,numOfColumns, 3, 1);
+            Console.WriteLine(treeCount);
+
+            Console.WriteLine("Commencing Day3 B...");
+            var c1 = TraverseMap(lines, numOfColumns, 1, 1);
+            var c2 = TraverseMap(lines, numOfColumns, 3, 1);
+            var c3 = TraverseMap(lines, numOfColumns, 5, 1);
+            var c4 = TraverseMap(lines, numOfColumns, 7, 1);
+            var c5 = TraverseMap(lines, numOfColumns, 1, 2);
+            double answer = c1 * c2 * c3 * c4 * c5;
+            Console.WriteLine(answer);
+        }
+        private static int TraverseMap(string[] lines, int numOfColumns, int rightSteps, int downSteps)
+        {
+            var row = 0;
+            var column = 0;
+            var treeCount = 0;
+            while (true)
+            {
+                row += downSteps;
+                if (row >= lines.Length) { 
+                    return treeCount; 
+                }
+                column = (column + rightSteps) % numOfColumns;
+                if (lines[row][column] == '#')
+                {
+                    treeCount++;
+                }
+            }
         }
     }
 }
