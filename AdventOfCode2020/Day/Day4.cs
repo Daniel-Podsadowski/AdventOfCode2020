6d47bf1eb91f100513383d58f59165053b89da4a
Daniel Podsadowski <pods0005@algonquinlive.com>
2020-12-03 20:10:40 -0500

    Prepared for Day4

---
+++ AdventOfCode2020/Day/Day4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Day4
+    {
+        public static void Main()
+        {
+            var lines = File.ReadLines(@"..\..\..\Data\day4.txt").ToArray();
+
+            Console.WriteLine("Commencing Day4 A...");
+            
+        }
+    }
+}
