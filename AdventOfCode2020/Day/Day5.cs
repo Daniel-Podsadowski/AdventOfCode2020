ba1dc04e487e1bcbdcfbc2118563c2c1c47d8d06
Daniel Podsadowski <pods0005@algonquinlive.com>
2020-12-06 22:54:40 -0500

    Started Day5 Part A
    I keep getting either 1 too high or too low on some test cases. I need to think of how to calculate the mid on the binary search tree

--- AdventOfCode2020/Day/Day5.cs
+++ AdventOfCode2020/Day/Day5.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace AdventOfCode2020
 {
@@ -6,8 +7,59 @@ namespace AdventOfCode2020
     {
         public static void Main()
         {
-            var lines = File.ReadAllLines(@"..\..\..\Data\day5.txt");
+            var seats = File.ReadAllLines(@"..\..\..\Data\test.txt");
 
+            int highestSeatId = 0;
+            foreach (var seat in seats)
+            {
+                var seatId = BinarySearchTree(seat);
+                if (seatId > highestSeatId)
+                {
+                    highestSeatId = seatId;
+                }
+            }
+            Console.WriteLine("Highest SeatId = " + highestSeatId);
+
+        }
+
+        private static int BinarySearchTree(string seat)
+        {
+            int row = 0, column = 0, low = 0, mid = 63, high = 127, seatId = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                var letter = seat[i];
+                if (letter == 'F')
+                {
+                    high = mid;
+                    mid = (int)Math.Ceiling(((decimal)low + high) / 2);
+                }
+                if (letter == 'B')
+                {
+                    low = mid;
+                    mid = (low + high) / 2;
+                }
+            }
+            row = mid;
+            low = 0;
+            mid = 4;
+            high = 7;
+            for (var i = 7; i < 10; i++)
+            {
+                var letter = seat[i];
+                if (letter == 'L')
+                {
+                    high = mid;
+                    mid = (int)Math.Ceiling(((decimal)low + high) / 2);
+                }
+                if (letter == 'R')
+                {
+                    low = mid;
+                    mid = (int)Math.Ceiling(((decimal)low + high) / 2);
+                }
+            }
+            column = mid;
+            seatId = (row * 8) + column;
+            return seatId;
         }
     }
 }
