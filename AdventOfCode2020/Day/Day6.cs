b2338cf1034480eb0bbb21839ca81a2dfd9c49f2
Daniel Podsadowski <pods0005@algonquinlive.com>
2020-12-08 20:30:50 -0500

    Complete Day 6, Prepare for Day 7

--- AdventOfCode2020/Day/Day6.cs
+++ AdventOfCode2020/Day/Day6.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
@@ -8,6 +11,57 @@ namespace AdventOfCode2020
         {
             var lines = File.ReadAllLines(@"..\..\..\Data\day6.txt");
 
-            
+            var sumOfCounts = 0;
+            var sumOfEveryoneCounts = 0;
+            var groupAnswers = "";
+            var numOfPeopleInGroup = 0;
+            List<char> groupAnswerList = new List<char>();
+            foreach (var line in lines)
+            {
+                line.Trim();
+                if (line == "")
+                {
+                    foreach (var character in groupAnswers)
+                    {
+                        if (!groupAnswerList.Contains(character))
+                        {
+                            groupAnswerList.Add(character);
+                        }
+                    }
+                    sumOfCounts += groupAnswerList.Count();
+                    
+                    string distinctLetters = "";
+                    foreach (var letter in groupAnswers)
+                    {
+                        if (!distinctLetters.Contains(letter))
+                        {
+                            distinctLetters += letter;
+                        }
+                    }
+
+                    var numOfGroupCounts = 0;
+                    foreach (var letter2 in distinctLetters)
+                    {
+                        if (groupAnswers.Count(x => x == letter2) == numOfPeopleInGroup)
+                        {
+                            numOfGroupCounts++;
+                        }
+                    }
+                    sumOfEveryoneCounts += numOfGroupCounts;
+
+                    groupAnswers = "";
+                    numOfPeopleInGroup = 0;
+                    groupAnswerList.RemoveRange(0, groupAnswerList.Count);
+                }
+                else
+                {
+                    if (groupAnswers == "") { groupAnswers = line; }
+                    else { groupAnswers += line; }
+                    numOfPeopleInGroup++;
+                }
+            }
+            Console.WriteLine("Sum of questions which were answered yes = " + sumOfCounts);
+            Console.WriteLine("Sum of questions which everyone answered = " + sumOfEveryoneCounts);
+        }
     }
 }
---
+++ AdventOfCode2020/Day/Day7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Day7
+    {
+        public static void Main()
+        {
+            var lines = File.ReadAllLines(@"..\..\..\Data\day7.txt");
+
+
+        }
+    }
+}
